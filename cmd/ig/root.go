2f61b14117f1aa2f86cb3acead28934d40da146e
mawngo <sitdownrightnow.dev@gmail.com>
2026-03-29 22:49:11 +0700

    perf: reuse page when handling both stories and posts

--- cmd/ig/root.go
+++ cmd/ig/root.go
@@ -21,17 +21,19 @@ func NewCmd() *cobra.Command {
 		Run: func(_ *cobra.Command, _ []string) {
 			start := time.Now()
 			_ = browser.Execute(f.Flags, func(p *browser.Page) error {
+				storiesLikedCnt, err := igbot.LikeStories(p, f.LikePostFlags)
+				if err != nil {
+					slog.Error("Error liking stories", slog.Any("err", err))
+					return nil
+				}
+
+				igbot.WaitBetweenArticle()
 				postLikedCnt, err := igbot.LikePosts(p, f.LikePostFlags)
 				if err != nil {
 					slog.Error("Error liking posts", slog.Any("err", err))
 					return nil
 				}
 
-				storiesLikedCnt, err := igbot.LikeStories(p, f.LikePostFlags)
-				if err != nil {
-					slog.Error("Error liking stories", slog.Any("err", err))
-					return nil
-				}
 				slog.Info("Completed",
 					slog.Int("posts", postLikedCnt),
 					slog.Int("stories", storiesLikedCnt),
